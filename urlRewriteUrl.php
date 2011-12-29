07d973f81b2ae076ab5fd3b39216e17ef7f16b1e
Jean-Cédric THÉROND <jean.cedric.t@gmail.com>
2011-12-29 09:49:41 +0100

    CTools exportable implementation - start

---
+++ urlRewriteUrl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file Embed the urlRewriteUrl class.
+ */
+
+/**
+ * Encapsulate the url to rewrite.
+ *
+ * @author jean-cedric
+ */
+class urlRewriteUrl {
+  //put your code here
+}
