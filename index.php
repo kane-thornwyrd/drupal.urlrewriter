ab6b63800626b5195c3626d577561125ab176c34
Jean-cédric THÉROND <jean.cedric.t@gmail.com>
2011-12-29 10:04:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-echo 'THIS IS A DRUPAL MODULE, NOT INTENDED TO BE EXECUTED ALONE !';
+function buzzz() {
+  echo 'THIS IS A DRUPAL MODULE, NOT INTENDED TO BE EXECUTED ALONE !'; 
+}
+
+while (TRUE) {
+  buzzz();
+}
